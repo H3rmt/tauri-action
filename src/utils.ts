3298ef1d561f6d3b4ffb5acf1f31e7a49121668e
Enrico Stemmer <100319043+H3rmt@users.noreply.github.com>
2022-11-26 21:04:54 +0000

    fixed artifacts

--- src/utils.ts
+++ src/utils.ts
@@ -23,23 +23,20 @@ export async function buildProject(
         cwd: root,
         stdio: 'inherit'
     })
-
-    const capName = name.charAt(0).toUpperCase() + name.slice(1)
-
     const artifactsPath = join(root, 'target', 'release', 'bundle')
 
     if (platform() === 'darwin') {
         return [
-            { path: join(artifactsPath, `dmg/${capName}_${version}_x64.dmg`), name: `${name}_${version}_x64.dmg` },
-            { path: join(artifactsPath, `macos/${capName}.app`), name: `${name}_${version}.app` },
-            { path: join(artifactsPath, `macos/${capName}.app.tar.gz`), name: `${name}_${version}.app.tar.gz` },
-            { path: join(artifactsPath, `macos/${capName}.app.tar.gz.sig`), name: `${name}_${version}.app.tar.gz.sig` }
+            { path: join(artifactsPath, `dmg/${name}_${version}_x64.dmg`), name: `${name}_${version}_x64.dmg` },
+            { path: join(artifactsPath, `macos/${name}.app`), name: `${name}_${version}.app` },
+            { path: join(artifactsPath, `macos/${name}.app.tar.gz`), name: `${name}_${version}.app.tar.gz` },
+            { path: join(artifactsPath, `macos/${name}.app.tar.gz.sig`), name: `${name}_${version}.app.tar.gz.sig` }
         ]
     } else if (platform() === 'win32') {
         return [
-            { path: join(artifactsPath, `msi/${capName}_${version}_x64_en-US.msi`), name: `${name}_${version}_x64_en-US.msi` },
-            { path: join(artifactsPath, `msi/${capName}_${version}_x64_en-US.msi.zip`), name: `${name}_${version}_x64_en-US.msi.zip` },
-            { path: join(artifactsPath, `msi/${capName}_${version}_x64_en-US.msi.zip.sig`), name: `${name}_${version}_x64_en-US.msi.zip.sig` }
+            { path: join(artifactsPath, `msi/${name}_${version}_x64_en-US.msi`), name: `${name}_${version}_x64_en-US.msi` },
+            { path: join(artifactsPath, `msi/${name}_${version}_x64_en-US.msi.zip`), name: `${name}_${version}_x64_en-US.msi.zip` },
+            { path: join(artifactsPath, `msi/${name}_${version}_x64_en-US.msi.zip.sig`), name: `${name}_${version}_x64_en-US.msi.zip.sig` }
         ]
     } else {
         return [
@@ -61,11 +58,11 @@ export async function publish(github: InstanceType<typeof GitHub>, releaseId: nu
     // })).data
 
     for (const artifact of artifacts) {
-        const headers = {
-            'content-type': 'application/zip',
-            'content-length': statSync(artifact.path).size
-        }
-        core.info(`headers["content-length"]:${headers["content-length"]}`)
+        // const headers = {
+        //     'content-type': 'application/zip',
+        //     'content-length': statSync(artifact.path).size
+        // }
+        // core.info(`headers["content-length"]:${headers["content-length"]}`)
 
         // const existingAsset = existingAssets.find((a) => a.name === artifact.name)
         // core.info(`existing: ${existingAsset}, name: ${artifact.name}`)
@@ -79,6 +76,7 @@ export async function publish(github: InstanceType<typeof GitHub>, releaseId: nu
         //     })
         // }
 
+        core.info(`uploading: ${artifact.name}`)
         await github.rest.repos.uploadReleaseAsset({
             // headers,
             owner: context.repo.owner,
